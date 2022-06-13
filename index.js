900aa846bf73ed8f654141b42bc155653054f686
nguyenducphucc <jonybounce@gmail.com>
2022-06-13 17:21:40 -0500

    initialcommit

--- index.js
+++ index.js
@@ -1,41 +1,33 @@
 const app = require("./app");
 const http = require("http");
 const config = require("./utils/config");
-const { v4: uuidv4 } = require("uuid");
-const webSocketServer = require("websocket").server;
+//const { WebSocketServer } = require("ws");
+//const ws = require("ws");
 
 const server = http.createServer(app);
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });
 
-const wsServer = new webSocketServer({
-  httpServer: server,
-});
+// const wss = new WebSocketServer({
+//   server,
+// });
 
-const clients = {};
-wsServer.on("request", (request) => {
-  var userID = uuidv4();
+// wss.on("connection", (client) => {
+//   console.log("New client connected!");
 
-  console.log(
-    new Date().toLocaleTimeString() +
-      " Received a new connection from origin " +
-      request.origin
-  );
+//   client.on("message", (msg) => {
+//     console.log(`Message: ${msg}`);
 
-  const connection = request.accept(null, request.origin);
-  clients[userID] = connection;
+//     for (const cli of wss.clients) {
+//       if (cli.readyState === ws.OPEN) {
+//         console.log("Send!");
+//         cli.send(msg);
+//       }
+//     }
+//   });
 
-  connection.on("message", (message) => {
-    if (message.type === "utf8") {
-      console.log("Received Message: ", message.utf8Data);
-
-      for (const key in clients) {
-        clients[key].sendUTF(message.utf8Data);
-        console.log("send Message to: ", clients[key]);
-      }
-    }
-  });
-
-  connection.on("close", () => {});
-});
+//   client.on("close", () => {
+//     console.log("Client has disconnected");
+//   });
+// });
