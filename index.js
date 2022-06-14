3668e3d00e1252e8da94115d82d8fcf41c7f0d1b
nguyenducphucc <jonybounce@gmail.com>
2022-06-14 11:08:17 -0500

    initialcommit

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const app = require("./app");
 const http = require("http");
 const config = require("./utils/config");
-//const { WebSocketServer } = require("ws");
-//const ws = require("ws");
 
 const server = http.createServer(app);
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });
 
-// const wss = new WebSocketServer({
-//   server,
-// });
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+io.on("connection", (socket) => {
+  console.log("Websocket connected!, userID: " + socket.id);
 
-// wss.on("connection", (client) => {
-//   console.log("New client connected!");
-
-//   client.on("message", (msg) => {
-//     console.log(`Message: ${msg}`);
-
-//     for (const cli of wss.clients) {
-//       if (cli.readyState === ws.OPEN) {
-//         console.log("Send!");
-//         cli.send(msg);
-//       }
-//     }
-//   });
-
-//   client.on("close", () => {
-//     console.log("Client has disconnected");
-//   });
-// });
+  socket.on("message", (data) => {
+    socket.broadcast.emit("message", data);
+  });
+});
